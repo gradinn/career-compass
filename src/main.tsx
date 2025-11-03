7116d06a01a6a1216eca24c8e5a45fbb39b2ddf5
Addison Douglas <addisondouglas850@gmail.com>
2025-11-03 10:56:14 -0800

    env vars

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,15 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { Auth0Provider } from '@auth0/auth0-react';
-const domain = import.meta.env.DOMAIN;
-const clientId = import.meta.env.CLIENTID;
+const domain = import.meta.env.VITE_DOMAIN;
+const clientId = import.meta.env.VITE_CLIENTID;
+console.log(domain, clientId)
 
 createRoot(document.getElementById('root')!).render(
   
    <Auth0Provider
-    domain={domain!}
-    clientId={clientId!}
+    domain={domain}
+    clientId={clientId}
     authorizationParams={{
       redirect_uri: window.location.origin
     }}
